f3c20173b1498b50ee51d11625ce0d6f7ea143cb
Nello Rizzo <nellorizzo@live.it>
2025-02-26 16:34:58 +0100

    Calcolatrice

---
+++ calculator/src/main/java/calculator/Program.java
@@ -0,0 +1,14 @@
+package calculator;
+
+public class Program {
+
+	public static void main(String[] args) {
+		// Una calcolatrice consente
+		// di effettuare le 4 operazioni dell'aritmetica
+		// in maniera che esse modifichino un accumulatore
+		// interno che è possibile leggere alla fine delle
+		// operazioni.
+
+	}
+
+}
