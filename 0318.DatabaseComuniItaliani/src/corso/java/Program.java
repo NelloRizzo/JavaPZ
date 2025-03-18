0f07b7cad164645e6e82c6bfbae744857862766b
Nello Rizzo <nellorizzo@live.it>
2025-03-18 17:11:58 +0100

    Popolamento del database

---
+++ 0318.DatabaseComuniItaliani/src/corso/java/Program.java
@@ -0,0 +1,62 @@
+package corso.java;
+
+import corso.java.entities.Area;
+import corso.java.entities.City;
+import corso.java.entities.Province;
+import corso.java.entities.Region;
+import jakarta.persistence.Persistence;
+
+public class Program {
+
+	private static final String FILE_NAME = "./comuni.csv";
+	private static final String PERSISTENCE_UNIT = "JPA_Comuni_Italiani";
+
+	public static void main(String[] args) {
+		var loader = new CitiesLoader(FILE_NAME);
+		var cities = loader.readFile();
+		System.out.println(cities.size());
+
+		var provinces = cities.stream() //
+				.map(City::getProvince) //
+				.distinct() //
+				.toList();
+		var regions = provinces.stream() //
+				.map(Province::getRegion) //
+				.distinct() //
+				.toList();
+		var areas = regions.stream() //
+				.map(Region::getArea) //
+				.distinct() //
+				.toList();
+		System.out.println(provinces.size());
+		System.out.println(regions.size());
+		System.out.println(areas.size());
+
+		var emf = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT);
+		var em = emf.createEntityManager();
+
+		var trans = em.getTransaction();
+		trans.begin();
+
+		areas.forEach(a -> em.persist(a));
+		regions.forEach(r -> {
+			r.setArea(em.find(Area.class, r.getArea().getId()));
+			em.persist(r);
+		});
+		provinces.forEach(p -> {
+			p.setRegion(em.find(Region.class, p.getRegion().getId()));
+			em.persist(p);
+		});
+		cities.forEach(c -> {
+			c.setProvince(em.find(Province.class, c.getProvince().getId()));
+			em.persist(c);
+		});
+		trans.commit();
+
+		var firstArea = em.find(Area.class, 1);
+		System.out.println(firstArea);
+		System.out.println(firstArea.getRegions().size());
+		firstArea.getRegions().forEach(System.out::println);
+	}
+
+}
--- 0318.DatabaseComuniItaliani/src/corso/java/entities/Region.java
+++ 0318.DatabaseComuniItaliani/src/corso/java/entities/Region.java
@@ -1,14 +1,20 @@
 package corso.java.entities;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @Data
 @AllArgsConstructor
@@ -24,4 +30,7 @@ public class Region {
 	private String name;
 	@ManyToOne
 	private Area area;
+	@OneToMany(mappedBy = "region", fetch = FetchType.LAZY)
+	@ToString.Exclude
+	private final Set<Province> provinces = new HashSet<>();
 }
