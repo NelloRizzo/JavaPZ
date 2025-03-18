e7074974f1b605c813c753331fca9658f88683b7
Nello Rizzo <nellorizzo@live.it>
2025-03-18 15:15:29 +0100

    Esercizio del 18.03

---
+++ 0318.DatabaseComuniItaliani/src/corso/java/entities/Area.java
@@ -0,0 +1,20 @@
+package corso.java.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder(setterPrefix = "with")
+
+@Entity
+@Table(name = "ripartizioni")
+public class Area {
+	private Integer id;
+	private String name;
+}
