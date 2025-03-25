69c48e817b2ab967fd748bcd764cb3caf5bfc67e
Nello Rizzo <nellorizzo@live.it>
2025-03-25 12:16:11 +0100

    Spring Web App

--- 0320.SpringWebIntro/src/main/java/corso/java/HelloController.java
+++ 0320.SpringWebIntro/src/main/java/corso/java/HelloController.java
@@ -2,6 +2,8 @@ package corso.java;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -11,5 +13,18 @@ public class HelloController {
 	@GetMapping("/hello")
 	public @ResponseBody String sayHello() {
 		return "Hello, World!";
+	}
+
+	// GET /hello-name?name=Nello&lang=it
+	@GetMapping("/hello-name")
+	public @ResponseBody String sayHelloByQuery(@RequestParam String name,
+			@RequestParam(defaultValue = "it") String lang) {
+		return String.format("%s, %s!", lang.equals("it") ? "Ciao" : "Hello", name == null ? "World" : name);
+	}
+
+	// GET /hello/Nello?lang=it
+	@GetMapping("/hello/{name}")
+	public @ResponseBody String sayHello(@PathVariable String name, @RequestParam(defaultValue = "it") String lang) {
+		return String.format("%s, %s!", lang.equals("it") ? "Ciao" : "Hello", name == null ? "World" : name);
 	}
 }
