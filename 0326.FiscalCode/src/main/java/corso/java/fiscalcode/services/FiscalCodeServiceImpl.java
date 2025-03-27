90bcea273f2cd8ac921f3907464d9464e34e0e82
Nello Rizzo <nellorizzo@live.it>
2025-03-27 12:06:55 +0100

    Prima implementazione del servizio di calcolo del codice fiscale.

--- 0326.FiscalCode/src/main/java/corso/java/fiscalcode/services/FiscalCodeServiceImpl.java
+++ 0326.FiscalCode/src/main/java/corso/java/fiscalcode/services/FiscalCodeServiceImpl.java
@@ -1,5 +1,7 @@
 package corso.java.fiscalcode.services;
 
+import java.time.LocalDate;
+
 import org.springframework.stereotype.Service;
 
 import corso.java.fiscalcode.controllers.models.PersonalData;
@@ -9,7 +11,33 @@ public class FiscalCodeServiceImpl implements FiscalCodeService{
 
 	@Override
 	public String generateFiscalCode(PersonalData data) {
-		return "XXXXXX00X00X000X";
+		var sb = new StringBuilder() //
+				.append(handleLastName(data.getLastName())) //
+				.append(handleFirstName(data.getFirstName())) //
+				.append(handleBirthday(data.getBirthday(), data.getGender())) //
+				.append(handleBirthCity(data.getBirthCity())) //
+				;
+		return sb.append(checkCode(sb)).toString();
+	}
+
+	private char checkCode(StringBuilder sb) {
+		return 'X';
+	}
+
+	private String handleBirthCity(String birthCity) {
+		return "X000";
+	}
+
+	private String handleBirthday(LocalDate birthday, char gender) {
+		return "00X00";
+	}
+
+	private String handleFirstName(String firstName) {
+		return "XXX";
+	}
+
+	private String handleLastName(String lastName) {
+		return "XXX";
 	}
 
 }
