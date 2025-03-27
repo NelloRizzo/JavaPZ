764723e3523bf52925bb1e44050991a823f789f1
Nello Rizzo <nellorizzo@live.it>
2025-03-27 11:58:48 +0100

    .

--- 0326.FiscalCode/src/main/java/corso/java/fiscalcode/controllers/web/FiscalCodeController.java
+++ 0326.FiscalCode/src/main/java/corso/java/fiscalcode/controllers/web/FiscalCodeController.java
@@ -3,6 +3,7 @@ package corso.java.fiscalcode.controllers.web;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import corso.java.fiscalcode.controllers.models.PersonalData;
 import corso.java.fiscalcode.services.FiscalCodeService;
@@ -22,10 +23,11 @@ public class FiscalCodeController {
 		return "index";
 	}
 
+	@PostMapping("/generate")
 	public String generateFiscalCode(PersonalData data, ModelMap map) {
 		var fc = service.generateFiscalCode(data);
 		map.addAttribute("data", data);
 		map.addAttribute("fiscalCode", fc);
-		return "show";
+		return "index";
 	}
 }
