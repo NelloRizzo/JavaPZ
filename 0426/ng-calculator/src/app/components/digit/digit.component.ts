46ff86046e69a53acfa3ca9bd5bc0b13f05ff023
Nello Rizzo <nellorizzo@live.it>
2025-04-24 11:46:20 +0200

    .

--- 0426/ng-calculator/src/app/components/digit/digit.component.ts
+++ 0426/ng-calculator/src/app/components/digit/digit.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-digit',
@@ -7,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrl: './digit.component.css'
 })
 export class DigitComponent {
-  digit: number = 0;
+  @Input() digit: number = 0;
 }
