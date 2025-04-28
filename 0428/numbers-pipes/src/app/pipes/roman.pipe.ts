1637562b84ae6b89821eb064cc8a56298235dbbe
Nello Rizzo <nellorizzo@live.it>
2025-04-28 14:49:55 +0200

    .

--- 0428/numbers-pipes/src/app/pipes/roman.pipe.ts
+++ 0428/numbers-pipes/src/app/pipes/roman.pipe.ts
@@ -7,25 +7,25 @@ export class RomanPipe implements PipeTransform {
 
   transform(value: number): string {
     if (value < 1 || value > 3999) return 'N/A';
-    const romanNumerals: { value: number, numeral: string }[] = [
-      { value: 1000, numeral: 'M' },
-      { value: 900, numeral: 'CM' },
-      { value: 500, numeral: 'D' },
-      { value: 400, numeral: 'CD' },
-      { value: 100, numeral: 'C' },
-      { value: 90, numeral: 'XC' },
-      { value: 50, numeral: 'L' },
-      { value: 40, numeral: 'XL' },
-      { value: 10, numeral: 'X' },
-      { value: 9, numeral: 'IX' },
-      { value: 5, numeral: 'V' },
-      { value: 4, numeral: 'IV' },
-      { value: 1, numeral: 'I' }
+    const romanliterals: { value: number, literal: string }[] = [
+      { value: 1000, literal: 'M' },
+      { value: 900, literal: 'CM' },
+      { value: 500, literal: 'D' },
+      { value: 400, literal: 'CD' },
+      { value: 100, literal: 'C' },
+      { value: 90, literal: 'XC' },
+      { value: 50, literal: 'L' },
+      { value: 40, literal: 'XL' },
+      { value: 10, literal: 'X' },
+      { value: 9, literal: 'IX' },
+      { value: 5, literal: 'V' },
+      { value: 4, literal: 'IV' },
+      { value: 1, literal: 'I' }
     ];
-    return romanNumerals.reduce((acc, n) => {
+    return romanliterals.reduce((acc, n) => {
       const numKey = n.value
       while (value >= numKey) {
-        acc += n.numeral;
+        acc += n.literal;
         value -= numKey;
       }
       return acc;
