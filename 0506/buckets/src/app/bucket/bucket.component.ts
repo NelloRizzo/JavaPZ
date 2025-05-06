e67098a73bf19a259268cf761ebcd166167d5148
Nello Rizzo <nellorizzo@live.it>
2025-05-06 11:24:25 +0200

    .

--- 0506/buckets/src/app/bucket/bucket.component.ts
+++ 0506/buckets/src/app/bucket/bucket.component.ts
@@ -11,4 +11,20 @@ export class BucketComponent {
 
   @Input()
   bucket?: Bucket
+
+  filling: boolean = false
+
+  startFilling() {
+    this.bucket?.add(1)
+    if (this.bucket?.isFull) this.filling = false
+    if (this.filling)
+      setTimeout(() => this.startFilling(), 100);
+  }
+  startFill() {
+    this.filling = true
+    this.startFilling()
+  }
+  stopFill() {
+    this.filling = false
+  }
 }
