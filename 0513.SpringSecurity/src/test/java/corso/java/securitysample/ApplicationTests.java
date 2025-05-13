09b45deb160ec764cf1bd75dbfd26bbda17ffb13
Nello Rizzo <NelloRizzo@users.noreply.github.com>
2025-05-13 09:33:55 +0200

    Spring Boot Security

---
+++ 0513.SpringSecurity/src/test/java/corso/java/securitysample/ApplicationTests.java
@@ -0,0 +1,13 @@
+package corso.java.securitysample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
